fc8a51188637ece908274458f465044059fb8c67
Ziffyyy <dreamofhallow@gmail.com>
2023-04-10 09:09:09 -0300

    Create operate function

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
+let lastNumber;
+let currentNumber;
+let operator;
 //Make the basic operations for the calculator
 function add(lastNumber, currentNumber) {
-    return lastNumber + currentNumber;
+    return currentNumber = lastNumber + currentNumber;
 }
 
 function subtract(lastNumber, currentNumber) {
-    return lastNumber - currentNumber;
+    return currentNumber = lastNumber - currentNumber;
 }
 
 function multiply(lastNumber,currentNumber) {
-    return lastNumber * currentNumber;
+    return currentNumber = lastNumber * currentNumber;
 }
 
 function divide(lastNumber, currentNumber) {
-    return lastNumber / currentNumber;
+    return currentNumber = lastNumber / currentNumber;
 }
+
+function operate(lastNumber, currentNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(lastNumber,currentNumber);
+        case '-':
+            return subtract(lastNumber, currentNumber);
+        case '*':
+            return multiply(lastNumber, currentNumber);
+        case '/':
+            return divide(lastNumber, currentNumber);
+        default:
+            console.log('something went wrong...'); 
+    }
+
+}
+
+console.log(operate(2, 8, '/'));
