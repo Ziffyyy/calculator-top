33bc606057a64657bbb3503c357d0f831c1cf8e5
Ziffyyy <dreamofhallow@gmail.com>
2023-04-10 08:52:20 -0300

    Initiate the project and add basic functions
    Added the html, js and css files. Added the four basic
    functions to operate the calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+//Make the basic operations for the calculator
+function add(lastNumber, currentNumber) {
+    return lastNumber + currentNumber;
+}
+
+function subtract(lastNumber, currentNumber) {
+    return lastNumber - currentNumber;
+}
+
+function multiply(lastNumber,currentNumber) {
+    return lastNumber * currentNumber;
+}
+
+function divide(lastNumber, currentNumber) {
+    return lastNumber / currentNumber;
+}
